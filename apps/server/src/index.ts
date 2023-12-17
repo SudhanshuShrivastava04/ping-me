4230d743b4ab147421f5fd6f3f3371b85e93c6f9
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2023-12-17 23:05:17 +0530

    project setup

---
+++ apps/server/src/index.ts
@@ -0,0 +1,20 @@
+import http from "http";
+import SocketService from "./services/socket";
+
+// init ports and server
+async function init() {
+  // create a new socket service
+  const socketService = new SocketService();
+
+  // create a new HTTP server
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+  //attach both HTTP and socket server
+  socketService.io.attach(httpServer);
+
+  httpServer.listen(PORT, () =>
+    console.log(`HTTP Server started at PORT:${PORT}`)
+  );
+}
+init();
